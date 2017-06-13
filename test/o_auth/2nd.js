78d53442ba439a433b8c47d5b147e005c95085e7
Filip Skokan <panva.ip@gmail.com>
2017-06-13 11:09:59 +0200

    prompt during OP-OAuth-*

--- test/o_auth/2nd.js
+++ test/o_auth/2nd.js
@@ -1,1 +1,11 @@
-it('OP-OAuth-2nd', require('../helpers').regular);
+const { navigate, testUrl, login, proceed, passed } = require('../helpers');
+
+it('OP-OAuth-2nd', async function () {
+  const test = this.test.title;
+
+  await navigate(testUrl(test));
+  await proceed();
+  await login();
+
+  await passed(test);
+});
--- test/o_auth/2nd_revokes.js
+++ test/o_auth/2nd_revokes.js
@@ -1,1 +1,11 @@
-it('OP-OAuth-2nd-Revokes', require('../helpers').regular);
+const { navigate, testUrl, login, proceed, passed } = require('../helpers');
+
+it('OP-OAuth-2nd-Revokes', async function () {
+  const test = this.test.title;
+
+  await navigate(testUrl(test));
+  await proceed();
+  await login();
+
+  await passed(test);
+});
