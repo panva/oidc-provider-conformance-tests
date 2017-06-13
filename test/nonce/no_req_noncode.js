39cd5fdeb94194d504d1441f671c77807433d4a2
Filip Skokan <panva.ip@gmail.com>
2017-06-13 10:46:42 +0200

    OP-nonce-NoReq-noncode with a prompt

--- test/nonce/no_req_noncode.js
+++ test/nonce/no_req_noncode.js
@@ -1,1 +1,9 @@
-it('OP-nonce-NoReq-noncode', require('../helpers').nointeraction);
+const { navigate, testUrl, proceed, passed } = require('../helpers');
+
+it('OP-nonce-NoReq-noncode', async function () {
+  const test = this.test.title;
+  await navigate(testUrl(test));
+  await proceed();
+
+  await passed(test);
+});
